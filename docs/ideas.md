640d84a033e501d279effef9e5b12ccf86c4db6a
dingfeng <fengDing0321@gmail.com>
2025-05-29 20:56:31 +0800

    Co-authored-by: Michael D. Adams <adamsmd@users.noreply.github.com>

---
+++ docs/ideas.md
@@ -0,0 +1,34 @@
+# Ideas
+
+- Background Markdown
+- Create folder of example .mmu
+- Move files into docs folder
+
+## Goals
+
+- Implement a nice language
+- Discover how to use the language well
+
+- Interpreter
+- Improve Syntax
+  - Fewer brackets
+  - Be more pretty
+  - Look at Haskell or OCaml
+  - No have to go through command within each mu. E.g., by designating a current continuation or current expression
+- Syntactic Sugar
+- Develop Examples
+  - Large example (e.g., a parser)
+  - Want to discover what idioms to use
+    - In particular find ways that the duality is actually an advantage
+      - Example, map and fold can be written in accumulator style or not
+- Letrec
+- Debugger
+- GraphViz to see evaluation
+- Side Effects (but that might be a bad idea)
+- Look at Duo for syntax examples
+- Protocol implementations
+- Multi-thread webserver (combine concurrency with protocols)
+- Static Type checker
+- Declare data types?
+  - Does this matter if we don't have a type checker?
+- Automated testing system
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,97 @@
+import Parser (parseString, parseFile)
+import Text.Megaparsec (errorBundlePretty)
+import Pretty
+import Syntax
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+-- TODO: convert to tests
+main = tests
+
+tests :: IO ()
+tests = do
+
+  putStrLn "\n TEST PRETTY PRINTER"
+
+  putStrLn "\n variables:"
+  printExpr (Var "x")
+  printExpr (Var "xyz")
+  printExpr (Var "x1")
+
+  putStrLn "\n Co-variables:"
+  printCoExpr (CoVar "k")
+  printCoExpr (CoVar "k1")
+
+  putStrLn "\n Constructors:"
+  printExpr (Cons "Nil" [])
+  printExpr (Cons "Pair" [Left (Var "x"), Right (CoVar "k")])
+  -- Cons (Head x) (Tail k)
+  let nestedCon1 = Cons "Cons" [
+                    Left (Cons "Head" [Left (Var "x")]),
+                    Right (CoCons "Tail" [Right (CoVar "k")])
+                  ]
+  printExpr nestedCon1
+  let nestedCon2 = Cons "Cons" [
+                    Left (Cons "Head" [Left (Var "x")]),
+                    Left (Cons "Cons" [
+                      Left (Cons "Head" [Left (Var "y")]),
+                      Right (CoCons "Tail" [Right (CoVar "k")])
+                    ])
+                  ]
+  printExpr nestedCon2
+
+  putStrLn "\n Command:"
+  printCommand (Command (Var "x") (CoVar "k"))
+  printCommand (Command (Cons "Cons" [Left (Var "x"), Left (Var "xs")]) (CoVar "k1"))
+  printCommand (Command (Cons "Cons" [Left (Var "x"), Left (Var "xs")]) (Mu []))
+  printCommand (Command (Cons "Cons" [Left (Var "x"), Left (Var "xs")]) (Mu [
+                (VarPattern "x", Command (Var "x") (CoVar "ret")),
+                (ConsPattern "Nil" [], Command (Cons "Done" []) (CoVar "ret")),
+                (ConsPattern "Cons" [Left "x", Right "xs"],
+                 Command (Var "x") (CoCons "Continue" [Right (CoVar "xs")]))
+              ]))
+
+  putStrLn "\n Pattern:"
+  putStrLn $ showPattern (ConsPattern "Cons" [Left "x", Right "k"])
+  putStrLn $ showPattern (VarPattern "abc")
+
+  putStrLn "\n Mu expression:"
+  let muExpr = Mu [
+                (VarPattern "x", Command (Var "x") (CoVar "ret")),
+                (ConsPattern "Nil" [], Command (Cons "Done" []) (CoVar "ret")),
+                (ConsPattern "Cons" [Left "x", Right "xs"],
+                 Command (Var "x") (CoCons "Continue" [Right (CoVar "xs")]))
+              ]
+  printCoExpr muExpr
+
+  putStrLn "\n CoMu expression:"
+  let comuExpr = CoMu [
+                  (CoConsPattern "Print" [Left "x"],
+                   Command (Var "x") (CoVar "k")),
+                  (CoVarPattern "halt",
+                   Command (Cons "Exit" []) (CoVar "k"))
+                ]
+  printExpr comuExpr
+
+  putStrLn "\n TEST PARSER"
+  let input = "< x |> ~k >"
+  case parseString input of
+    Left err -> putStrLn $ errorBundlePretty err
+    Right cmd -> print cmd  
+  
+  let input1 = "< ListCons x xs |> ~k >"
+  case parseString input1 of
+    Left err -> putStrLn $ errorBundlePretty err
+    Right cmd -> print cmd 
+
+  let input2 = "< ListCons x xs |> mu [ ListNil -> < x |> ~k > | ListCons x xs -> < x |> ~xs > ] >"
+  case parseString input2 of
+    Left err -> putStrLn $ errorBundlePretty err
+    Right cmd -> print cmd
+
+  let sampleAdd = "< comu[ ~Ap3 x y ~k -> < x |> mu[ Z -> < y |> ~k > | S x' -> < add |> ~Ap3 x' (S y) ~k > ] > ] |> ~Ap3 (S Z) (S (S Z)) k0 >"
+  case parseString sampleAdd of
+    Left err -> putStrLn $ errorBundlePretty err
+    Right cmd -> do 
+      print cmd
+      printCommand cmd
+  return ()
